86f7c7f977c3404d68e2cced11f66a23e63d3b5f
25S01( Ali Tasawar) <tasawar@chuo-ai.jp>
2026-05-20 03:30:52 +0000

    ali

--- src/mcd-order-front.jsx
+++ src/mcd-order-front.jsx
@@ -24,7 +24,7 @@ function McdOrderFront() {
       id: 2,
       name: "チーズバーガー",
       price: 250,
-      image: "/images/cb1.webp",
+      image: "/images/cb1.png",
       description: "チーズ入り"
     },
     {
